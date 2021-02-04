f99799f2f0a2750e38e08e790ee87010996d0553
faisalhatila <hatilas1994@gmail.com>
2021-02-04 19:56:02 +0500

    project initiated

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <!-- <li v-for="resource in storedResources">
+          <p></p>
+      </li> -->
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The officcial Vue.js documentaion',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
